3e14b5369fb17db59194310073caa1466cd48ea3
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-07-27 23:48:41 -0300

    update 27/07/2022

---
+++ curso-react/fundamentos-react/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+
+import Primeiro from './components/basicos/Primeiro';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    <div>
+      <Primeiro />
+    </div>
+  
+);
