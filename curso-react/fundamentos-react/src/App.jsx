637c76eb1006c636a361a0f82972ea5f20013f80
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-07-29 23:03:33 -0300

    update-29/07/2022

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css"
 
+import Card from "./components/layout/Card"
+
 import Fragmento from "./components/basicos/Fragmento"
 import ComParametro from "./components/basicos/ComParametro"
 import Primeiro from "./components/basicos/Primeiro"
 import Aleatorio from "./components/basicos/Aleatorio"
-import Card from "./components/layout/Card"
+import Familia from "./components/basicos/Familia"
+import FamiliaMembro from "./components/basicos/FamiliaMembro"
 
 export default function App() {
     return (
@@ -12,20 +15,27 @@ export default function App() {
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
-                <Card titulo="#04 - Desafio Aleatório">
+                <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                    <Familia sobrenome="Silva">
+                        <FamiliaMembro nome="Agnaldo" />
+                        <FamiliaMembro nome="Camila" />
+                        <FamiliaMembro nome="Paty"  />
+                    </Familia>
+                </Card>
+                <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                     <Aleatorio min={1} max={60} />
                 </Card>
-                <Card titulo="#03 - Fragmento">
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
                     <Fragmento />
                 </Card>
-                <Card titulo="#02 - Com Parâmetro">
+                <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
                     <ComParametro
                         titulo="Situação do aluno"
                         aluno="Pedro Silva"
                         nota={9.3}
                     />
                 </Card>
-                <Card titulo="#01 - Primeiro Componente">
+                <Card titulo="#01 - Primeiro Componente" color="#588C73">
                     <Primeiro></Primeiro>
                 </Card>
             </div>
---
+++ curso-react/fundamentos-react/src/components/basicos/Familia.jsx
@@ -0,0 +1,12 @@
+import React, { cloneElement } from "react";
+
+export default function Familia(props) {
+    return (
+        <div>
+            {props.children.map((child, i) => {
+                    return cloneElement(child, {...props, key: i})
+                })
+            }
+        </div>
+    )
+}
