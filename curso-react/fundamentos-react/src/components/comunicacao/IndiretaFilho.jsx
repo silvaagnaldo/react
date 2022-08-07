4e59a3c27c6618c698ea5db5327b19943903a4f9
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-08-07 17:31:21 -0300

    update - 07/08/22

--- curso-react/fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ curso-react/fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -1,12 +1,13 @@
 export default function IndiretaPai(props) {
+    const cb = props.quandoClicar
+    
+    const gerarIdade = () => parseInt(Math.random() * ( 20)) + 50
+    const gerarNerd = () => Math.random() > 0.5
+
     return(
         <div>
                 <div>Filho</div>
-                <button onClick={
-                    function(e) {
-                        props.quandoClicar('João', 53, true)
-                    }
-            }>
+                <button onClick={(e) => cb('João', gerarIdade(), gerarNerd())}>
                 Fornecer Informações</button>
         </div>
     )
