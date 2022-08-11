e8d89ee9afd135d4c61752ff8a2a9ffece8cf807
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-08-10 22:54:10 -0300

    update - 10/08/22

--- curso-react/calculadora/src/components/Button.jsx
+++ curso-react/calculadora/src/components/Button.jsx
@@ -1,7 +1,17 @@
+import React from 'react'
 import './Button.css'
 
 export default function Button(props) {
-    return(
-        <button className="Button">{props.label}</button>
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
     )
 }
