f05f838d7bba85cd757bdab04162e164de071277
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-08-18 22:42:47 -0300

    update - 18/08/22

--- curso-react/hooks/src/views/examples/UseEffect.jsx
+++ curso-react/hooks/src/views/examples/UseEffect.jsx
@@ -4,12 +4,14 @@ import { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+// Ex #01
 function calcFatorial(num) {
     const n = parseInt(num)
     if(n < 0) return -1
     if(n === 0) return 1
     return calcFatorial(n - 1) * n
 }
+
 
 const UseEffect = (props) => {
     const [number, setNumber] = useState(1)
@@ -25,6 +27,13 @@ const UseEffect = (props) => {
         }
     }, [fatorial])
 
+    //Ex #02
+    const [status, setStatus] = useState("Ímpar")
+
+    useEffect(function() {
+        setStatus(number % 2 === 0 ? "Par" : "Ímpar")
+    }, [number])
+
     return (
         <div className="UseEffect">
             <PageTitle
@@ -37,11 +46,14 @@ const UseEffect = (props) => {
                     <span className='text'>Fatorial: </span>
                     <span className='text red'>{fatorial === -1 ? 'Não existe' : fatorial}</span>
                 </div>
-                <input type="number" className="input" vallue={number} onChange={e => setNumber(e.target.value)}/>
+                <input type="number" className="input" value={number} onChange={e => setNumber(e.target.value)}/>
             </div>
             <SectionTitle title="Exercício #02"/>
             <div className='center'>
-
+               <div>
+                    <span className='text'>status: </span>
+                    <span className='text red'>{status}</span>
+               </div>
             </div>
         </div>
     )
