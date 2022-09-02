13c6bc63c06e3c692b3845376c039e69b0e8eca7
Agnaldo Silva <Agnaldo_ss@hotmail.com>
2022-09-01 22:56:24 -0300

    update-01/09/22

--- curso-react/aulao_nextjs/next-crud/src/pages/index.tsx
+++ curso-react/aulao_nextjs/next-crud/src/pages/index.tsx
@@ -11,6 +11,14 @@ export default function Home() {
     new Cliente('Pedro', 54, '4')
   ]
 
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
   return (
     <div className={
       `flex justify-center items-center h-screen
@@ -18,7 +26,9 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <Tabela clientes={clientes}/>
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado}
+        clienteExcluido={clienteExcluido}
+        />
       </Layout>
     </div>
       
